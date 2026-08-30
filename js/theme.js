d1561e0a7f6c3ee2e44c62bcc1ef2d53391cc166
Jeppe O <100990116+OlanderDesign@users.noreply.github.com>
2026-08-30 22:43:01 +0200

    dark mode væk

--- js/theme.js
+++ js/theme.js
@@ -4,29 +4,6 @@ AOS.init({
   offset: 40
 });
 
-const html = document.documentElement;
-const toggle = document.getElementById("themeToggle");
-const icon = toggle.querySelector("i");
-
-function setTheme(theme) {
-  html.setAttribute("data-bs-theme", theme);
-  localStorage.setItem("theme", theme);
-
-  if (theme === "dark") {
-    icon.className = "bi bi-sun-fill";
-  } else {
-    icon.className = "bi bi-moon-stars-fill";
-  }
-}
-
-const savedTheme = localStorage.getItem("theme");
-setTheme(savedTheme || "light");
-toggle.addEventListener("click", () => {
-  const currentTheme = html.getAttribute("data-bs-theme");
-  const nextTheme = currentTheme === "dark" ? "light" : "dark";
-  setTheme(nextTheme);
-});
-
 const navbar = document.querySelector('.navbar-blur');
 const hero = document.querySelector('.hero-image');
 
